177ed38124ec3832f9829236c3f5dcd9a4ffa438
Mel Allen <melcuetara.mac>
2022-07-08 15:52:15 +0800

    UserTYpe

--- app/src/main/java/com/example/rentaland/ui/signup/UserTypeActivity.java
+++ app/src/main/java/com/example/rentaland/ui/signup/UserTypeActivity.java
@@ -2,13 +2,44 @@ package com.example.rentaland.ui.signup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.rentaland.databinding.ActivityUserTypeBinding;
 
 public class UserTypeActivity extends AppCompatActivity {
 
+    private ActivityUserTypeBinding binding;
+
+    private String userType;
+    private Bundle bundle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user_type);
+        binding = ActivityUserTypeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        bundle = new Bundle();
+
+        if (binding.rbFarmer.isChecked()) {
+            userType = "user_farmer";
+        } else {
+            userType = "user_investor";
+        }
+        bundle.putString("userType", userType);
+        binding.btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!binding.cbAgree.isChecked()) {
+                    Toast.makeText(UserTypeActivity.this, "Please Agree to The Terms and Conditions!", Toast.LENGTH_SHORT).show();
+                    binding.cbAgree.setError("");
+                    return;
+                }
+                Intent intent = new Intent(UserTypeActivity.this, SignUpCredentialsActivity.class);
+                intent.putExtras(bundle);
+                startActivity(intent);
+            }
+        });
     }
 }
