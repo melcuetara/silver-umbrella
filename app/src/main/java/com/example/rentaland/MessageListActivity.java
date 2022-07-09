8fe4c3d50a686fd8572134aae48932056b746490
LENOVO <francisandriemercado.1@gmail.com>
2022-07-09 09:43:58 +0800

    Message

--- app/src/main/java/com/example/rentaland/MessageListActivity.java
+++ app/src/main/java/com/example/rentaland/MessageListActivity.java
@@ -9,6 +9,6 @@ public class MessageListActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_message_list2);
+        setContentView(R.layout.activity_message_list);
     }
 }
